da0beee59e1aec18429eb1b1cad2926a9e0b2eae
AmritDeep <amritdeepdhungana@hotmail.com>
2016-02-11 10:57:45 -0600

    test for string containg 1

--- test_calculator.rb
+++ test_calculator.rb
@@ -5,9 +5,14 @@ require 'pry'
 describe Calculator do
 	let(:calculator) { Calculator.new }
 
-	it "returns 0 for an empty  string" do
-		result = calculator.calculate("")
+	it "should returns 0 for an empty  string" do
+		result = calculator.calculate("")	
 		expect(result).to eq(0)
+	end
+
+	it "should returns 1 for a string containing 1" do
+		result = calculator.calculate("1")
+		expect(result).to eq(1)
 	end
 	
 end
