7b49f00f02d8de2d271215008809f8637892d3ca
AmritDeep <amritdeepdhungana@hotmail.com>
2016-02-11 11:16:00 -0600

    create method to convert and calculate sum and call them iin calculate method

--- calculator.rb
+++ calculator.rb
@@ -4,12 +4,25 @@ class Calculator
 
 	def calculate(input)
 		if input.include?(',')
-			string = input.split(',')
-			number = string.map { |x| x.to_i }
-			number.inject { |sum, n| sum + n }
+			# string = input.split(',')
+			# number = string.map { |x| x.to_i }
+			number = convert_string_to_integers(input)
+			calculate_sum(number)
 		else
 			input.to_i
 		end
+	end
+
+
+	private
+
+	def convert_string_to_integers(input)
+		string = input.split(',')
+		number = string.map { |x| x.to_i }	
+	end
+
+	def calculate_sum(number)
+		number.inject { |sum, n| sum + n }
 	end
 
 end
