6e87450e2686e10f4e35c7bdfb33f54a2325ec51
AmritDeep <amritdeepdhungana@hotmail.com>
2016-02-11 11:10:49 -0600

    Handle for calcuatio for single parameter as well as multiple parameter

--- calculator.rb
+++ calculator.rb
@@ -3,9 +3,13 @@ require 'pry'
 class Calculator
 
 	def calculate(input)
-		string = input.split(',')
-		number = string.map { |x| x.to_i }
-		number.inject { |sum, n| sum + n }
+		if input.include?(',')
+			string = input.split(',')
+			number = string.map { |x| x.to_i }
+			number.inject { |sum, n| sum + n }
+		else
+			input.to_i
+		end
 	end
 
 end
