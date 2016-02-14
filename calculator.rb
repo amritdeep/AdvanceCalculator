ff2423e4b84b9767883b525893b455a6fd0135a4
AmritDeep <amritdeepdhungana@hotmail.com>
2016-02-14 01:07:46 -0600

    create method to check operator to add and subtract number

--- calculator.rb
+++ calculator.rb
@@ -14,17 +14,29 @@ class Calculator
 		end
 	end
 
-	def check_operator(input)
+	def check_multiple_operator(input)
+		new_array = []
 		array = input.split(' ')
-		a = []	
-		array.each_index do |i|
-			if array[i].include?('+')
-				a = array[i-1].to_i + array[i+1].to_i
-			end
-		end
-		return a
-	end
+		if array.count >= 3
+			array.each_index do |i|
+				# del_list = [array[i], array[i+1], array[i+2]]
+				del_list = [array[0], array[1], array[2]]
+				a = del_list[0].to_i + del_list[2].to_i if del_list.include?('+')
+				a = del_list[0].to_i - del_list[2].to_i if del_list.include?('-')
+				a = a.to_s
+				new_array.push(a)
 
+				del_list.each do |del|
+					array.delete_at(array.index(del))
+				end
+
+				array.unshift(new_array[0])
+				del_list.clear
+				new_array.clear
+			end
+			array.first.to_i	
+		end	
+	end
 
 	private
 
--- test_calculator.rb
+++ test_calculator.rb
@@ -26,13 +26,29 @@ describe Calculator do
 	end
 
 	it "should check for operator like +" do
-		result = cal.check_operator("1 + 2")
+		# result = cal.check_operator("1 + 2")
+		result = cal.check_multiple_operator("1 + 2")
 		expect(result).to eql(3)
 	end
 
-	it "should check for double operator like +, -" do
-		result = cal.calculate("5 + 2 - 1")
+	it "should add two number like 2 + 5" do
+		result = cal.check_multiple_operator("2 + 5")
+		expect(result).to eql(7)
+	end
 
+	it "should subtract two number like 5 - 3" do
+		result = cal.check_multiple_operator("5 - 3")
+		expect(result).to eql(2)
+	end
+
+	it "should subtract two number like 2 - 6" do
+		result = cal.check_multiple_operator("2 - 6")
+		expect(result).to eql(-4)
+	end
+
+	it "should check for double operator like +, -" do
+		# result = cal.calculate("5 + 2 - 1")
+		result = cal.check_multiple_operator("5 + 2 - 1")
 		expect(result).to eql(6)
 	end
 	
